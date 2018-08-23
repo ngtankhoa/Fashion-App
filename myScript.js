640f49b9035fef7413d6f7fdc35c464f0b2ed30b
ngtankhoa <khoant.uit@gmail.com>
2018-08-23 14:39:48 +0700

    xong phần slide, đến phần thumbnail

--- myScript.js
+++ myScript.js
@@ -8,3 +8,19 @@ function closeNav() {
     document.getElementById("main").style.marginRight = "0";
 }
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
