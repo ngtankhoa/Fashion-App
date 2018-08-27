cdfc8bb51210b5ddd76b8303bfdcd5e48dfc5869
ngtankhoa <khoant.uit@gmail.com>
2018-08-27 09:55:47 +0700

    tạm hoàn thành thumbnail slider

--- myScript.js
+++ myScript.js
@@ -1,18 +1,37 @@
+var slides = document.getElementsByClassName("mySlides");
+var slideIndex = 0;
+var i, interval, timeoutSlider, timeoutSidenav;
+var sideNavOpen = 'false';
+var thumbContainer = document.getElementById("thumbnail");
+var thumbHeight = document.getElementsByClassName("thumb")[1];
+thumbHeight = thumbHeight.scrollHeight + 6; //temporary know that margin top is 6px
+window.scrollBy(60, 60);
+
 function openNav() {
+    sideNavOpen = 'true';
+    clearTimeout(timeoutSidenav);
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginRight = "250px";
+    pauseSlider();
+    timeoutSidenav = setTimeout(closeNav, 10000);
 }
 
 function closeNav() {
+    sideNavOpen = 'false';
+    resumeSlider();
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("main").style.marginRight = "auto";
 }
 
-var slides = document.getElementsByClassName("mySlides");
-var slideIndex = 0;
-var i;
-var interval;
-var timeout;
+// for arrow pointer
+function plusSlides() {
+    showSlides(slideIndex);
+}
+
+function subSlides() {
+    slideIndex--;
+    showSlides(slideIndex);
+}
 
 function immediatelyShowSlides() {
     for (i = 0; i < slides.length; i++) {
@@ -24,26 +43,44 @@ function immediatelyShowSlides() {
     slides[slideIndex - 1].style.display = "block";
 }
 
+function scrollThumbnail() {
+    thumbContainer.scrollTop = thumbHeight;
+}
+
 function showSlides(n) {
     slideIndex = n;
     immediatelyShowSlides();
-    interval = setInterval(immediatelyShowSlides, 3000);
+    interval = setInterval(function () {
+        immediatelyShowSlides();
+        scrollThumbnail();
+    }, 3000);
 }
 
 function thumb_showSlides(n) {
     clearInterval(interval);
-    clearTimeout(timeout);
+    clearTimeout(timeoutSlider);
     showSlides(n);
 }
 
-// bat su kien mouseenter va mouseleave
-document.getElementById("slider").onmouseenter = function () {
-    clearInterval(interval);
-    clearTimeout(timeout);
+function resumeSlider() {
+    timeoutSlider = setTimeout(showSlides, 3000, slideIndex);
+    console.log("resume");
 };
 
-document.getElementById("slider").onmouseleave = function () {
-    timeout = setTimeout(showSlides, 3000, slideIndex);
+function pauseSlider() {
+    clearInterval(interval);
+    clearTimeout(timeoutSlider);
+    console.log("pause");
+};
+
+// bat su kien mouseenter va mouseleave
+document.getElementById("slider").onmouseenter = pauseSlider;
+
+document.getElementById("slider").onmouseleave = function(){
+    if (sideNavOpen==='false') resumeSlider();
 };
 
 showSlides(0);
+
+//hoc jQuery de lam animation cho scroll
+//them noi dung vao bang json
