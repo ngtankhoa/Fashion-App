3291a8dfa08a5018fbcbd50e036017b6508e7c2b
ngtankhoa <khoant.uit@gmail.com>
2018-09-25 12:27:06 +0700

    fix lỗi, chuyển sang dùng ES6 function

--- myScript.js
+++ myScript.js
@@ -1,18 +1,4 @@
-$(document).ready(function () {
-    //kiểm tra có tổng cộng bao nhiêu hình và sinh slide mới
-    // var tmpURLSlider = "http://localhost:1337/images/model/model";
-    // var tmpURLThumbnail = "http://localhost:1337/images/thumbnail/thumbnail";
-    // var slidesLength = 0, boo = 'true';
-    // do {
-    //     $.get("http://localhost:1337/images/model/model" + slidesLength)
-    //     .done(function () {
-    //         slidesLength;
-    //     }).fail(function () {
-    //         boo = 'false';
-    //     });
-
-    // } while (boo === 'true');
-
+$(document).ready(() => {
     //khai báo các biến ban đầu
     var $slides = $(".mySlides");
     var slideshowContainer = $(".slideshow-container");
@@ -20,88 +6,73 @@ $(document).ready(function () {
     var sideNavOpen = 'false';
     var sliderPauseTime = 3000, pauseTimeSideNav = 10000;
     var closeBtn = $(".closebtn");
-    var prev = $(".prev, #arrowup");
-    var next = $(".next, #arrowdown");
+    var scrollUp = $(".arrowup"), scrollDown = $(".arrowDown");
+    var slideUp = $("#slideUp"), slideDown = $("#slideDown");
+
     var languageEN = $("#en"), languageVI = $("#vi");
 
-    function openNav() {
+    openNav = () => {
         sideNavOpen = 'true';
         clearTimeout(timeoutSidenav);
         document.getElementById("mySidenav").style.width = "27%";
-        document.getElementById("").style.width = "50vw";
-        // timeoutSidenav = setTimeout(closeNav, pauseTimeSideNav);
+        document.getElementById("main").style.marginRight = "20vw";
+        timeoutSidenav = setTimeout(closeNav, pauseTimeSideNav);
     }
 
-    function closeNav() {
+    closeNav = () => {
         sideNavOpen = 'false';
         document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginRight = "auto";
+        document.getElementById("main").style.marginRight = "auto"; //chố này có vấn đề với transition
         resumeSlider();
     }
-
-    function showSlides(n) {
-        slideIndex = n;
-        console.log("index trong show:" + slideIndex);
-        $slides.css("display", "none");
-        $slides.eq(slideIndex).css("display", "block");
-        if (slideIndex < $slides.length - 1) { slideIndex++; } //độ dài của $slides là 4
-        else { slideIndex = 0; }
-        // timeoutSlider = setTimeout(showSlides, sliderPauseTime, slideIndex);
-    }
-
-    function showSlidesNgc(n) {
-        console.log(slideIndex);
-        slideIndex = n;
-        if (slideIndex == -1) { slideIndex = 3; }
-        else if (slideIndex == 0) { slideIndex = 1; }
-        else if (slideIndex == 1) { slideIndex = 2; }
-        else if (slideIndex == -2) { slideIndex = 0; };
-        console.log("index trong show:" + slideIndex);
-        $slides.css("display", "none");
-        $slides.eq(slideIndex).css("display", "block");
-        if (slideIndex == 1) {
-            slideIndex = slideIndex - 1;
-        }
-        // timeoutSlider = setTimeout(showSlides, sliderPauseTime, slideIndex);
-    }
-
-    function pauseSlider() {
+    pauseSlider = () => {
         clearTimeout(timeoutSlider);
         console.log("pause");
     }
 
-    function resumeSlider() {
+    resumeSlider = () => {
         if (sideNavOpen === 'false')
             timeoutSlider = setTimeout(showSlides, sliderPauseTime, slideIndex);
         console.log("resume");
     }
 
+    showSlides = (n) => {
+        slideIndex = n;
+        $slides.css("display", "none");
+        $slides.eq(slideIndex).css("display", "block");
+        if (slideIndex < $slides.length - 1) { slideIndex++; } //độ dài của $slides là 4
+        else { slideIndex = 0; }
+        timeoutSlider = setTimeout(showSlides, sliderPauseTime, slideIndex);
+    }
+
+
     //bắt sự kiện mouseenter và mouseleave để quản lí slider
-    slideshowContainer.mouseenter(function () { pauseSlider(); }).mouseleave(function () { resumeSlider(); });
+    slideshowContainer.mouseenter(() => { pauseSlider(); }).mouseleave(() => { resumeSlider(); });
 
     //tạm thời cho tất cả icon cùng bắt 1 sự kiện openNav()
     $(".accessory").click(openNav);
     closeBtn.click(closeNav);
 
-    //bắt sự kiện click 4 arrow
-    prev.click(function () {
-        clearTimeout(timeoutSlider);
-        console.log("truoc:" + slideIndex);
-        var tmp = slideIndex - 2;
-        console.log("tmp: " + tmp);
-        showSlidesNgc(tmp);
-        console.log(slideIndex);
+    //bắt sự kiện click scrollUp và scrollDown
+    scrollUp.click(() => {
+
     });
-    next.click(function () {
-        clearTimeout(timeoutSlider);
-        showSlides(slideIndex);
+    scrollDown.click(() => {
+    });
+
+    //bắt sự kiện click slideUp và slideDown
+    slideUp.click(() => {
+
+    });
+    slideDown.click(() => {
+
     });
 
     //bắt sự kiện nút chuyển đổi ngôn ngữ
-    languageVI.click(function () {
+    languageVI.click(() => {
 
     });
-    languageEN.click(function () {
+    languageEN.click(() => {
 
     });
 
@@ -110,12 +81,11 @@ $(document).ready(function () {
 
 });
 
-//nạp json chuyển đổi ngôn ngữ
-//dùng hàm lấy dữ liệu từ file local
 
-//nạp json dữ liệu đầu vào của từng icon
-//(tạm thời 2 cái này lấy dữ liệu từ local trước đã)
-
-//sửa function nút prev
 //làm animation cho slide
 //viết lại function cho thumbnail slider
+
+//nạp json chuyển đổi ngôn ngữ
+//dùng hàm lấy dữ liệu từ file local
+//nạp json dữ liệu đầu vào của từng icon
+//(tạm thời 2 cái này lấy dữ liệu từ local trước đã)
